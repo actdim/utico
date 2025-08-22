8a9f493bb90d2fe19953d2878710ed7641c49274
Pavel Borodaev <pavel.borodaev@gmail.com>
2025-08-22 18:26:35 +0200

    memoEffect test

---
+++ tests/utils.test.ts
@@ -0,0 +1,47 @@
+import { describe, it, expect, vi } from "vitest";
+import { delayAsync, delayErrorAsync, withTimeoutAsync, memoEffect } from "@/utils";
+
+describe("msgBus", () => {
+    process.on("unhandledRejection", (reason, promise) => {
+        console.error("Unhandled Rejection at:", promise, "reason:", reason);
+        process.exit(1);
+    });
+
+    // window.onerror = (message, source, lineno, colno, error) => {
+    //     console.error("Caught error:", error);
+    // };
+
+    // window.onunhandledrejection = (event) => {
+    //     console.error("Unhandled rejection:", event.reason);
+    // };
+
+    it("can call memoEffect", () => {
+        let dep = "test";
+        let id = 0;
+        let c = 0;
+        const action = (a: string, fake = false) => {
+            if (!fake) {
+                id++;
+            }
+            return a + id;
+        };
+        const fn = memoEffect(
+            () => dep,
+            (str) => {
+                c++;
+                return action(str);
+            }
+        );
+        
+        let r = fn();
+        expect(r).toBe(action(dep, true));
+        let oldR = r;
+        r = fn();
+        expect(r).toBe(oldR);
+        expect(c).toBe(1);
+        dep = "new";
+        r = fn();
+        expect(r).toBe(action(dep, true));
+        expect(c).toBe(2);
+    });
+});
